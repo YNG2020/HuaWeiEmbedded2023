7d7e1b96b454d8e12970e7a5a196b278037ea172
YNG2020 <69188798+YNG2020@users.noreply.github.com>
2023-04-16 10:09:38 +0800

    初步版本的生成随机输入的程序，是一个函数，函数的参数皆为引用

---
+++ generateRandomNet.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+int NMAX = 5000, NMIN = 2;
+int MMAX = 5000, MMIN = 2;
+int TMAX = 10000, TMIN = 2;
+int PMAX = 80, PMIN = 2;
+int DMAX = 1000, DMIN = 2;
+
+void generateRandomNet(vector<vector<int>>& rEdge, vector<vector<int>>& rBus, int& rN, int& rM, int& rT, int& rP, int& rD) {
+
+	srand(time(NULL) + rand() % 1000);  // 设置随机数种子  
+	rN = rand() % (NMAX - NMIN + 1) + NMIN;  // 生成随机数  
+	rM = rand() % (MMAX - MMIN + 1) + MMIN;  // 生成随机数  
+	rT = rand() % (TMAX - TMIN + 1) + TMIN;  // 生成随机数  
+	rP = rand() % (PMAX - PMIN + 1) + PMIN;  // 生成随机数  
+	rD = rand() % (DMAX - DMIN + 1) + DMIN;  // 生成随机数
+
+	rEdge.resize(rM);
+	for (auto& one : rEdge) {
+		one.resize(3);
+
+		srand(time(NULL) + rand() % 1000);  // 设置随机数种子 
+		int s = rand() % rN;  // 生成随机数 1
+		srand(time(NULL) + rand() % 1000);  // 设置随机数种子 
+		int t = rand() % rN;   // 生成随机数 2  
+  
+		do {   // 如果两个随机数相同，则重新生成 
+			if (s != t)
+				break;
+			srand(time(NULL) + rand() % 1000);  // 设置随机数种子 
+			t = rand() % rN;
+		} while(true);
+
+		int d = rand() % rD + 1;	// 保证距离大于0且不大于rD
+		one[0] = s;
+		one[1] = t;
+		one[2] = d;
+
+	}
+
+	rBus.resize(rT);
+	for (auto& one : rBus) {
+		one.resize(2);
+
+		srand(time(NULL) + rand() % 1000);  // 设置随机数种子 
+		int S = rand() % rN;  // 生成随机数 1
+		srand(time(NULL) + rand() % 1000);  // 设置随机数种子 
+		int T = rand() % rN;   // 生成随机数 2  
+  
+		do {   // 如果两个随机数相同，则重新生成 
+			if (S != T)
+				break;
+			srand(time(NULL) + rand() % 1000);  // 设置随机数种子 
+			T = rand() % rN;
+		} while(true);
+
+		one[0] = S;
+		one[1] = T;
+
+	}
+
+
+}
