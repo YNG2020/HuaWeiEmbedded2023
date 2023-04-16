9aee88a0bfc792ad4384ac8979b8671b281d6761
YNG2020 <69188798+YNG2020@users.noreply.github.com>
2023-04-16 10:56:21 +0800

    让所有的顶点都有一条边连到编号为0的顶点，强行保证图的连通

--- generateRandomNet.h
+++ generateRandomNet.h
@@ -15,32 +15,46 @@ void generateRandomNet(vector<vector<int>>& rEdge, vector<vector<int>>& rBus, in
 
 	srand(time(NULL) + rand() % 1000);  // 设置随机数种子  
 	rN = rand() % (NMAX - NMIN + 1) + NMIN;  // 生成随机数  
-	rM = rand() % (MMAX - MMIN + 1) + MMIN;  // 生成随机数  
+	//rM = rand() % (MMAX - MMIN + 1) + MMIN;  // 生成随机数  
 	rT = rand() % (TMAX - TMIN + 1) + TMIN;  // 生成随机数  
 	rP = rand() % (PMAX - PMIN + 1) + PMIN;  // 生成随机数  
 	rD = rand() % (DMAX - DMIN + 1) + DMIN;  // 生成随机数
 
+	int rM1 = min(MMAX, 3 * rN);
+	rM = rM1 + rN - 1;
+
 	rEdge.resize(rM);
 	for (auto& one : rEdge) {
 		one.resize(3);
+	}
+
+	for (int i = 0; i < rM1; ++i) {
 
 		srand(time(NULL) + rand() % 1000);  // 设置随机数种子 
 		int s = rand() % rN;  // 生成随机数 1
 		srand(time(NULL) + rand() % 1000);  // 设置随机数种子 
 		int t = rand() % rN;   // 生成随机数 2  
-  
+
 		do {   // 如果两个随机数相同，则重新生成 
 			if (s != t)
 				break;
 			srand(time(NULL) + rand() % 1000);  // 设置随机数种子 
 			t = rand() % rN;
-		} while(true);
+		} while (true);
 
 		int d = rand() % rD + 1;	// 保证距离大于0且不大于rD
-		one[0] = s;
-		one[1] = t;
-		one[2] = d;
+		rEdge[i][0] = s;
+		rEdge[i][1] = t;
+		rEdge[i][2] = d;
 
+	}
+
+	for (int i = rM1; i < rM; ++i) {
+
+		int d = rand() % rD + 1;	// 保证距离大于0且不大于rD
+		rEdge[i][0] = 0;
+		rEdge[i][1] = i - rM1 + 1;
+		rEdge[i][2] = d;
 	}
 
 	rBus.resize(rT);
@@ -55,6 +69,7 @@ void generateRandomNet(vector<vector<int>>& rEdge, vector<vector<int>>& rBus, in
 		do {   // 如果两个随机数相同，则重新生成 
 			if (S != T)
 				break;
+
 			srand(time(NULL) + rand() % 1000);  // 设置随机数种子 
 			T = rand() % rN;
 		} while(true);
